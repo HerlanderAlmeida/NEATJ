14ad687f8505796bbbd7d5165a260cc2ca86ab4e
SpicyKitten <rathavilash@utexas.edu>
2019-05-12 23:35:21 -0500

    Population to using list

---
+++ src/genetic/Main.java
@@ -0,0 +1,18 @@
+package genetic;
+
+import temp.IndividualChild;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Population<IndividualChild> p = new Population<IndividualChild>(10, IndividualChild::new);
+		boolean b = p.remove("blah");
+		System.out.println(b);
+		b = p.remove(p.get(0));
+		System.out.println(b);
+		for(int i = 0; i < 0; ++i)
+			p.indivs.remove(0);
+		System.out.println(p);
+	}
+}
--- src/genetic/Population.java
+++ src/genetic/Population.java
@@ -1,62 +1,86 @@
 package genetic;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 /**
  * It is possible, but not recommended, to use the types here to confuse people.
  * A population consists of a fixed size pool of Individuals.
  */
-public class Population<T extends Individual<? super T>> implements Supplier<Individual<?>[]>
+public class Population<T extends Individual<? super T>> implements Supplier<List<Individual<?>>>
 {
-	final Individual<?>[] indivs;
+	final List<Individual<?>> indivs;
 	
 	private Population(int size)
 	{
-		indivs = new Individual[size];
+		indivs = new ArrayList<Individual<?>>(size);
 	}
 	
 	public Population(int n, T instantiable)
 	{
 		this(n);
-		for(int i = 0; i < indivs.length; ++i)
-			indivs[i] = (Individual<?>) instantiable.getInstance();
+		IntStream.range(0, n).forEach(i -> indivs.add((Individual<?>)instantiable.getInstance()));
 	}
 	
 	public Population(int n, Supplier<T> supplier)
 	{
 		this(n);
-		for(int i = 0; i < indivs.length; ++i)
-			indivs[i] = supplier.get();
+		IntStream.range(0, n).forEach(i -> indivs.add(supplier.get()));
 	}
 	
-	public Individual<?>[] get()
+	public void addAll(Collection<? extends Individual<?>> c)
+	{
+		indivs.addAll(c);
+	}
+	
+	public boolean removeAll(Collection<?> c)
+	{
+		return indivs.removeAll(c);
+	}
+	
+	public boolean add(Individual<?> i)
+	{
+		return indivs.add(i);
+	}
+	
+	public boolean remove(Object o)
+	{
+		return indivs.remove(o);
+	}
+	
+	public Individual<?> get(int i)
+	{
+		return indivs.get(i);
+	}
+	
+	public List<Individual<?>> get()
 	{
 		return indivs;
 	}
 	
 	public int size()
 	{
-		return indivs.length;
+		return indivs.size();
 	}
 	
 	public Population<?> join(Population<?> other)
 	{
 		Population<?> joined = new Population<>(this.size() + other.size());
-		for(int i = 0; i < this.size(); ++i)
-			joined.indivs[i] = this.indivs[i];
-		for(int i = 0; i < other.size(); ++i)
-			joined.indivs[i + this.size()] = other.indivs[i];
+		joined.indivs.addAll(this.indivs);
+		joined.indivs.addAll(other.indivs);
 		return joined;
 	}
 	
 	public String toString()
 	{
 		if(this.size() > 7)
-			return String.format("Population:[%s, %s, %s, ... %d more Individuals ..., %s, %s, %s]",
-				indivs[0], indivs[1], indivs[2], size() - 6, indivs[size() - 3], indivs[size() - 2],
-				indivs[size() - 1]);
-		return String.format("Population:%s", Arrays.toString(indivs));
+			return String.format("Population: [%s, %s, %s, ... %d more Individuals ..., %s, %s, %s], size: %d",
+				indivs.get(0), indivs.get(1), indivs.get(2), size() - 6, indivs.get(size() - 3), indivs.get(size() - 2),
+				indivs.get(size() - 1), indivs.size());
+		return String.format("Population: %s", indivs);
 	}
 	
 }
