6f4f897bde04b68a67dc428830c5637a024de84d
SpicyKitten <rathavilash@gmail.com>
2020-07-17 23:13:43 -0500

    Tests randomly stopped being able to be run :( Module opened to others

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,8 @@
-module avi
+open module avi
 {
 	exports temp;
 	exports genetic;
 	
 	requires org.junit.jupiter.api;
 	requires com.google.gson;
-		opens network to com.google.gson;
-		opens network.neuron to com.google.gson;
-		opens network.connection to com.google.gson;
 }
